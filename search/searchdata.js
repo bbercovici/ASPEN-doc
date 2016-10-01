80f20a6aaf894e9ec0b9758b8b3994b9d97aaf1d
Benjamin Bercovici <bbercovici@Benjamins-MBP.hsd1.co.comcast.net>
2016-09-30 19:08:02 -0600

    added mainpage

--- search/searchdata.js
+++ search/searchdata.js
@@ -1,24 +1,18 @@
 var indexSectionsWithContent =
 {
-  0: "acdeglrsv~",
-  1: "arv",
-  2: "acdeglrsv~",
-  3: "ev"
+  0: "a",
+  1: "a"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "classes",
-  2: "functions",
-  3: "typedefs"
+  1: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Classes",
-  2: "Functions",
-  3: "Typedefs"
+  1: "Pages"
 };
 
